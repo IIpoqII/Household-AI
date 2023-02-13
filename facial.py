21f217c3c5d78e607414114427fc57f02f81c968
Anuj Venkatesan <anujvenkatesan@gmail.com>
2023-02-13 18:37:05 -0500

    organized into function

--- facial.py
+++ facial.py
@@ -1,8 +1,15 @@
 import cv2 as cv
 import face_recognition
-image = face_recognition.load_image_file("C:\\Users\\anujv\\Downloads\\Anuj.jpg")
-test = face_recognition.load_image_file("C:\\Users\\anujv\\Downloads\\stock.jpg")
-face_encodings = face_recognition.face_encodings(image)[0]
-test_encoding = face_recognition.face_encodings(test)[0]
-print(face_recognition.face_distance([face_encodings],test_encoding))
-print(face_recognition.compare_faces([face_encodings],test_encoding))
+def compare_faces(images,test):
+    face_encodings = []
+    for i in images:
+        image = face_recognition.load_image_file(i)
+        face_encodings.append(face_recognition.face_encodings(image)[0])
+    test = face_recognition.load_image_file("C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\test2.jpg")
+    
+    test_encoding = face_recognition.face_encodings(test)[0]
+    print(face_recognition.face_distance(face_encodings,test_encoding))
+    return face_recognition.compare_faces(face_encodings,test_encoding)
+images= ["C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\Anuj.jpg"]
+test = "C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\test2.jpg"
+print(compare_faces(images,test))
