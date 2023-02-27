bf544a16d3aa8b3815be6818f1610caa1f8c6f0e
Anuj Venkatesan <anujvenkatesan@gmail.com>
2023-02-27 18:33:12 -0500

    added instant camera to facial recogntion

--- facial.py
+++ facial.py
@@ -5,11 +5,25 @@ def compare_faces(images,test):
     for i in images:
         image = face_recognition.load_image_file(i)
         face_encodings.append(face_recognition.face_encodings(image)[0])
-    test = face_recognition.load_image_file("C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\test2.jpg")
+    
     
     test_encoding = face_recognition.face_encodings(test)[0]
     print(face_recognition.face_distance(face_encodings,test_encoding))
     return face_recognition.compare_faces(face_encodings,test_encoding)
 images= ["C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\Anuj.jpg"]
-test = "C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\test2.jpg"
-print(compare_faces(images,test))
+
+#print(compare_faces(images,test))
+vid = cv.VideoCapture(0)
+while(True):
+    
+    ret, img = vid.read()
+  
+#     # Display the resulting frame
+    cv.imshow('frame', img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+print(compare_faces(images,img))
+
+  
+
+
