372e174a2ef09e57d5b08bdc77c6e8337e721e74
Anuj Venkatesan <anujvenkatesan@gmail.com>
2023-04-12 17:10:58 -0400

    started user interface

--- facial.py
+++ facial.py
@@ -10,19 +10,19 @@ def compare_faces(images,test):
     test_encoding = face_recognition.face_encodings(test)[0]
     print(face_recognition.face_distance(face_encodings,test_encoding))
     return face_recognition.compare_faces(face_encodings,test_encoding)
-images= ["C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\Anuj.jpg"]
+# images= ["C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\Anuj.jpg"]
 
-#print(compare_faces(images,test))
-vid = cv.VideoCapture(0)
-while(True):
+
+# vid = cv.VideoCapture(0)
+# while(True):
     
-    ret, img = vid.read()
+#     ret, img = vid.read()
   
-#     # Display the resulting frame
-    cv.imshow('frame', img)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-print(compare_faces(images,img))
+# #     # Display the resulting frame
+#     cv.imshow('frame', img)
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#         break
+# print(compare_faces(images,img))
 
   
 
