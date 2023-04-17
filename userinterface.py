7f54d6e397b01bc31a73b7b4bf579166556f6398
Anuj Venkatesan <anujvenkatesan@gmail.com>
2023-04-17 17:49:24 -0400

    added gestures page

--- userinterface.py
+++ userinterface.py
@@ -2,29 +2,45 @@ import wx
 from facial import compare_faces
 import cv2 as cv
 import face_recognition
-class MyFrame(wx.Frame):    
+class MyFrame(wx.Frame):  
+      
     def __init__(self):
         super().__init__(parent=None, title='Household AI')
         panel = wx.Panel(self)
-        my_btn = wx.Button(panel, label='Login', pos=(5, 55))
-        my_btn.Bind(wx.EVT_BUTTON, self.face)
+        login = wx.Button(panel, label='Login', pos=(5, 55))
+        login.Bind(wx.EVT_BUTTON, self.face)
         self.Show()
+       
+    def gestures(self):
+        super().__init__(parent=None, title='Gestures')
+        gestures = wx.Panel(self)
+        logout = wx.Button(gestures, label='Logout', pos=(5, 55))
+        logout.Bind(wx.EVT_BUTTON, self.logout)
+        self.Show()
+    def logout(self,event):
+        self.Hide()
+        self.__init__()
     def face(self,event):
         vid = cv.VideoCapture(0)
+        wx.MessageBox('Press space to take picture', 'Household AI')
         while(True):
     
             ret, img = vid.read()
   
 #     # Display the resulting frame
             cv.imshow('frame', img)
-            if cv.waitKey(1) & 0xFF == ord('q'):
+            if cv.waitKey(1) & 0xFF == ord(' '):
                 break
         images= ["C:\\Users\\anujv\\OneDrive\\Pictures\\Camera Roll\\Anuj.jpg"]
         
         if(any(compare_faces(images,img))):
-            wx.MessageBox('login succeeded', 'Household AI')    
+            wx.MessageBox('login succeeded', 'Household AI')
+            cv.destroyAllWindows()
+            self.Hide()
+            self.gestures()
         else:
             wx.MessageBox('identity not authorized', 'Error',wx.ICON_ERROR)
+            cv.destroyAllWindows()
             
 
 
