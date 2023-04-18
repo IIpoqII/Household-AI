a87124b4906ef30f19340606481584fb10b0bd60
Lyscil <120293384+Lyscil@users.noreply.github.com>
2023-04-18 19:59:38 -0400

    Update RotateHandGesture.py

--- RotateHandGesture.py
+++ RotateHandGesture.py
@@ -4,21 +4,22 @@ import HandTracker
 
 
 class RotateHandGesture():
-    def __init__(self, volume=50):
+    def __init__(self, volume=50, flip_direction=False):
         self.ht = HandTracker.HandRecognition()
         self.volume = volume
+        self.flip_direction = flip_direction
 
     def draw_gesture(self, img, x1, y1, x2, y2, x3, y3):
         cv2.line(img, (x1, y1), (x2, y2), (255, 127, 0), 3)
         cv2.line(img, (x2, y2), (x3, y3), (255, 127, 0), 3)
 
-    def detect(self, lmList, lower_bound=0.30, upper_bound=0.75):
+    def detect(self, lm_list, lower_bound=0.30, upper_bound=0.75):
+        if not lm_list:
+            return False
         ratio = 0
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        x3, y3 = lmList[12][1], lmList[12][2]
-        # cv2.line(img, (x1, y1), (x2, y2), (255, 127, 0), 3)
-        # cv2.line(img, (x2, y2), (x3, y3), (255, 127, 0), 3)
+        x1, y1 = lm_list[4][1], lm_list[4][2]
+        x2, y2 = lm_list[8][1], lm_list[8][2]
+        x3, y3 = lm_list[12][1], lm_list[12][2]
         len1 = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
         len2 = math.sqrt(math.pow(x2 - x3, 2) + math.pow(y2 - y3, 2))
         if len1 != 0:
@@ -34,7 +35,10 @@ class RotateHandGesture():
         if x1 - x3 != 0:
             m2 = ((y1 - y3) / (x1 - x3))
         if 1 + (m2 * m1) != 0:
-            angle = math.atan((m1 - m2) / (1 + (m1 * m2)))
+            if not self.flip_direction:
+                angle = math.atan((m2 - m1) / (1 + (m2 * m1)))
+            else:
+                angle = math.atan((m1 - m2) / (1 + (m1 * m2)))
             angle = round(math.degrees(angle))
         return angle
 
@@ -42,26 +46,29 @@ class RotateHandGesture():
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
         cv2.line(img, (x1, y1), (x3, y3), (255, 127, 0), 3)
 
-    def execute(self, capture, lmList, handNum, threshold=True):
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[12][1], lmList[12][2]
+    def execute(self, capture, lm_list, hand_num, threshold=True):
+        x1, y1 = lm_list[4][1], lm_list[4][2]
+        x2, y2 = lm_list[12][1], lm_list[12][2]
         while threshold:
             success, img = capture.read()
+            img = cv2.flip(img, 1)
             img = self.ht.track_hand(img)
-            if self.ht.count_hands() != handNum + 1:
+            if self.ht.count_hands() != hand_num + 1:
                 break
-            lmList = self.ht.get_landmarks_positions(img)[handNum]
+            lm_list = self.ht.get_landmarks_positions()[hand_num]
 
             # processes and executes instruction
-            x3, y3 = lmList[12][1], lmList[12][2]
+            x3, y3 = lm_list[12][1], lm_list[12][2]
+            # cv2.line(img, (x1, y1), (x2, y2), (255, 127, 0), 3)
+            # cv2.line(img, (x2, y2), (x3, y3), (255, 127, 0), 3)
             angle = self.get_angle(x1, y1, x2, y2, x3, y3)
             new_volume = self.volume_control(img, self.volume, angle)
             if self.volume != new_volume:
                 self.volume = new_volume
-                x1, y1 = lmList[4][1], lmList[4][2]
-                x2, y2 = lmList[12][1], lmList[12][2]
+                x1, y1 = lm_list[4][1], lm_list[4][2]
+                x2, y2 = lm_list[12][1], lm_list[12][2]
 
-            threshold = self.detect(lmList)
+            threshold = self.detect(lm_list)
             self.draw_angle(img, x1, y1, x2, y2, x3, y3)
             cv2.imshow("Camera", img)
             cv2.waitKey(1)
@@ -81,5 +88,4 @@ class RotateHandGesture():
         # draws visual
         cv2.rectangle(img, (20, 20), (40, 120), (255, 127, 0), 2)
         cv2.rectangle(img, (20, 120 - volume), (40, 120), (255, 127, 0), -1)
-
         return volume
