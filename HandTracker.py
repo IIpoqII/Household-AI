30ee84ca7c93680e79b27306d85253c8cbf8cd04
Lyscil <120293384+Lyscil@users.noreply.github.com>
2023-04-18 19:55:12 -0400

    Update HandTracker.py

--- HandTracker.py
+++ HandTracker.py
@@ -1,11 +1,12 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import types
 from google.protobuf.json_format import MessageToDict
 
 
 class HandRecognition():
-    def __init__(self, mode=False, max_hands=2, complexity=1, detection_confidence=0.6, tracking_confidence=0.6):
+    def __init__(self, mode=False, max_hands=4, complexity=1, detection_confidence=0.6, tracking_confidence=0.6):
         self.results = None
 
         self.mode = mode
@@ -15,75 +16,86 @@ class HandRecognition():
         self.trackingConf = tracking_confidence
 
         self.mpHands = mp.solutions.hands
-        self.recognized_hands = self.mpHands.Hands(self.mode, self.maxHands, self.complexity,
-                                                   self.detectionConf, self.trackingConf)
+        self.recognized_hands = self.mpHands.Hands(self.mode, self.maxHands, self.complexity, self.detectionConf, self.trackingConf)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def count_hands(self):
-        if self.results.multi_hand_landmarks:
-            return len(self.results.multi_handedness)
-        return 0
+        # hands_list stores hand objects with attributes lm_list, orientation, handedness
+        self.hands_list = []
 
     def track_hand(self, img, draw=True, cirRad=5, cirBGR=(255, 127, 0)):
         # convert camera feed to RGB and identify hand
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.recognized_hands.process(imgRGB)
+        self.hands_list = []
 
         # visualize multiple hand
         if self.results.multi_hand_landmarks:
-            for handNum in self.results.multi_hand_landmarks:
+            # get data on each hand
+            multi_hand_landmarks = self.results.multi_hand_landmarks
+            multi_handedness_list = self.results.multi_handedness
+            for i in range(len(multi_hand_landmarks)):
+                hand_num = multi_hand_landmarks[i]
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handNum, self.mpHands.HAND_CONNECTIONS)
+                    self.mpDraw.draw_landmarks(img, hand_num, self.mpHands.HAND_CONNECTIONS)
+                hand = types.SimpleNamespace()
 
-                    # identifies landmarks on each hand
-                    for lmID, lm in enumerate(handNum.landmark):
-                        h, w, c = img.shape
-                        cx, cy = int(lm.x * w), int(lm.y * h)
-
+                # get hand landmarks
+                lm_list = []
+                for lmID, lm in enumerate(hand_num.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lm_list.append([lmID, cx, cy])
+                    if draw:
                         # highlight root of palm, tip of thumb, tip of index, tip of middle, tip of ring, tip of pinky
                         if (lmID == 0) or (lmID == 4) or (lmID == 8) or (lmID == 12) or (lmID == 16) or (lmID == 20):
                             cv2.circle(img, (cx, cy), cirRad, cirBGR, cv2.FILLED)
+                hand.lm_list = lm_list
+
+                # get hand orientation
+                pts = np.asarray([lm_list[0], lm_list[5], lm_list[17]])
+                normal_vector = (np.cross(pts[2] - pts[0], pts[1] - pts[2])).astype('float64')
+                normal_vector /= np.linalg.norm(normal_vector)
+                hand.orientation = normal_vector
+
+                # get handedness
+                label = MessageToDict(multi_handedness_list[i])['classification'][0]['label']
+                if label == "Left":
+                    hand.handedness = "Left"
+                elif label == "Right":
+                    hand.handedness = "Right"
+                else:
+                    hand.handedness = "Error"
+                self.hands_list.append(hand)
         return img
 
-    def get_hand_orientation(self, img):
-        lmList = self.get_landmarks_positions(img)
-        pts = np.asarray([lmList[0], lmList[5], lmList[17]])
-        normal_vector = (np.cross(pts[2]-pts[0], pts[1]-pts[2])).astype('float64')
-        normal_vector /= np.linalg.norm(normal_vector)
-        return normal_vector
+    def count_hands(self):
+        return len(self.hands_list)
 
-    def get_landmarks_positions(self, img):
-        lmList = []
-        hand = []
+    def get_hands(self):
+        return self.hands_list
 
-        if self.results.multi_hand_landmarks:
-            for handNum in self.results.multi_hand_landmarks:
-                hand = []
-                for lmID, lm in enumerate(handNum.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    hand.append([lmID, cx, cy])
-                lmList.append(hand)
-            return lmList
+    def get_landmarks_positions(self):
+        # hands_list[] is a list of landmarks, which is a list of the landmark ID and position
+        # hands_list[] is a list[list[]]
+        hands_list = []
+        for hand in self.hands_list:
+            hands_list.append(hand.lm_list)
+        return hands_list
 
-        # if landmark list is empty, return all IDs as 0
-        for lmID in range(20):
-            hand.append([lmID, 0, 0])
-        lmList.append(hand)
-        return lmList
+    def get_hand_orientation(self):
+        # hands_list[] is a list of orientations for each hand
+        hands_list = []
+        for hand in self.hands_list:
+            hands_list.append(hand.orientation)
+        return hands_list
 
     def get_handedness(self):
-        handedness = []
-        if self.results.multi_hand_landmarks:
-            for handNum in self.results.multi_handedness:
-                label = MessageToDict(handNum)['classification'][0]['label']
-                if label == "Left":
-                    handedness.append("Left")
-                elif label == "Right":
-                    handedness.append("Right")
-                else:
-                    handedness.append("Error")
-        return handedness
+        # hands_list[] is a list of handedness for each hand
+        hands_list = []
+        for hand in self.hands_list:
+            hands_list.append(hand.handedness)
+        return hands_list
+
 
 def main():
     cam_width, cam_height = 1280, 720
@@ -96,11 +108,11 @@ def main():
         success, img = capture.read()
         img = cv2.flip(img, 1)
         img = ht.track_hand(img)
-        lmList = ht.get_landmarks_positions(img)
-        # for hand in lmList:
-        #     print("Hand", lmList.index(hand), "   ", hand)
-        # print()
-        print(ht.get_handedness())
+        hands_list = ht.get_hands()
+        for hand in hands_list:
+            print("Hand", hands_list.index(hand), "   ", hand.lm_list[0])
+        if hands_list:
+            print()
 
         cv2.imshow("Camera", img)
         cv2.waitKey(1)
