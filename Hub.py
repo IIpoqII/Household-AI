dee59621cb1a6fc70ea8198a024e3309c0c19190
Lyscil <120293384+Lyscil@users.noreply.github.com>
2023-04-19 01:24:07 -0400

    Update Hub.py

--- Hub.py
+++ Hub.py
@@ -3,7 +3,7 @@ import mysql.connector
 import HandTracker
 import RotateHandGesture
 import SliderGesture
-import ExpandFrameGesture
+import ResizeFrameGesture
 
 
 cam_width, cam_height = 1280, 720
@@ -13,7 +13,7 @@ capture.set(4, cam_height)
 ht = HandTracker.HandRecognition()
 RotateHand = RotateHandGesture.RotateHandGesture()
 Slider = SliderGesture.SliderGesture()
-ExpandFrame = ExpandFrameGesture.ExpandFrameGesture()
+ResizeFrame = ResizeFrameGesture.ResizeFrameGesture()
 
 while True:
     success, img = capture.read()
@@ -22,11 +22,19 @@ while True:
     cv2.imshow("Camera", img)
     cv2.waitKey(1)
 
-    # detect one-handed gestures
     hands_list = ht.get_hands()
-    for hand in hands_list:
-        lm_list = hand.lm_list
-        if RotateHand.detect(lm_list):
-            RotateHand.execute(capture, lm_list, hands_list.index(hand))
-        if Slider.detect(lm_list):
-            Slider.execute(capture, lm_list, hands_list.index(hand))
+    hand_count = ht.count_hands()
+    if len(hands_list) > 0:
+        # detect one-handed gestures
+        for hand in hands_list:
+            lm_list = hand.lm_list
+            if RotateHand.detect(lm_list):
+                RotateHand.execute(capture, lm_list, hands_list.index(hand))
+            if Slider.detect(lm_list):
+                Slider.execute(capture, lm_list, hands_list.index(hand))
+        # detect multi-handed gestures
+        # if len(hands_list) > 1:
+        #     for hand1 in hands_list:
+        #         for hand2 in hands_list:
+        #             if ResizeFrame.detect(hand1, hand2):
+        #                 ResizeFrame.execute(capture, hand1, hand2, hand_count)
