6d4262de2efb22c5eb0e8898f5e83318ef25c528
Lyscil <120293384+Lyscil@users.noreply.github.com>
2023-04-23 16:09:17 -0400

    Update RotateHandGesture.py

--- RotateHandGesture.py
+++ RotateHandGesture.py
@@ -5,12 +5,13 @@ import time
 
 
 class RotateHandGesture:
-    def __init__(self, volume=50, invert_controls=False):
+    def __init__(self, default_value=50, invert_controls=False):
         self.ht = HandTracker.HandRecognition()
-        self.volume = volume
+        self.bar_height = default_value
         self.invert_controls = invert_controls
 
     def detect(self, lm_list, lower_bound=0.45, upper_bound=0.75):
+        # threshold is the ratio of the distance between the thumb and pointer fingers and the thumb and middle fingers
         if not lm_list:
             return False
         ratio = 0
@@ -21,7 +22,20 @@ class RotateHandGesture:
         len2 = math.sqrt(math.pow(x2 - x3, 2) + math.pow(y2 - y3, 2))
         if len1 != 0:
             ratio = round(len2 / len1, 2)
-        return lower_bound <= ratio <= upper_bound
+        if lower_bound <= ratio <= upper_bound:
+            # avoid exceptions
+            # (pointer and middle are touching thumb)
+            dist1 = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y3 - y1, 2))
+            dist2 = math.sqrt(math.pow(x3 - x1, 2) + math.pow(y3 - y1, 2))
+            if dist1 < 15 and dist2 < 15:
+                return False
+            # (pointer and middle are beneath thumb)
+            x4, y4, = lm_list[2][1], lm_list[2][2]
+            dist1 = math.sqrt(math.pow(x2 - x4, 2) + math.pow(y3 - y4, 2))
+            dist2 = math.sqrt(math.pow(x3 - x4, 2) + math.pow(y3 - y4, 2))
+            if dist1 < 30 and dist2 < 30:
+                return False
+            return True
 
     def execute(self, capture, lm_list, hand_num, hand_count, scale=2, min_incr=1, threshold=True):
         # scale affects the physical_movement:rotation ratio
@@ -29,10 +43,9 @@ class RotateHandGesture:
         incr = 0
         x1, y1 = lm_list[4][1], lm_list[4][2]
         x2, y2 = lm_list[12][1], lm_list[12][2]
+        # provide a 0.1-second buffer when the threshold fails
         start_time = time.time()
-        while (threshold and -min_incr < incr < min_incr) or (time.time() - start_time < 1):
-            if threshold:
-                start_time = time.time()
+        while (threshold and -min_incr < incr < min_incr) or (time.time() - start_time < 0.1):
             success, img = capture.read()
             img = cv2.flip(img, 1)
             img = self.ht.track_hand(img)
@@ -45,7 +58,7 @@ class RotateHandGesture:
             angle = self.get_angle(x1, y1, x2, y2, x3, y3)
             incr = angle / scale
             if incr <= -min_incr or incr >= min_incr:
-                self.volume_control(img, incr)
+                self.rotate_hand_control(img, incr)
                 x1, y1 = lm_list[4][1], lm_list[4][2]
                 x2, y2 = x3, y3
 
@@ -53,22 +66,24 @@ class RotateHandGesture:
             threshold = self.detect(lm_list)
             cv2.imshow("Camera", img)
             cv2.waitKey(1)
+            if threshold:
+                start_time = time.time()
         return
 
-    def volume_control(self, img, incr, max_vol=100, min_vol=0, knob_incr=1):
+    def rotate_hand_control(self, img, incr, max_vol=100, min_vol=0, knob_incr=1):
         # increments volume
         incr = round(incr * knob_incr)
-        self.volume = self.volume + incr
+        self.bar_height = self.bar_height + incr
 
         # bounds volume
-        if self.volume > max_vol:
-            self.volume = max_vol
-        if self.volume < min_vol:
-            self.volume = min_vol
+        if self.bar_height > max_vol:
+            self.bar_height = max_vol
+        if self.bar_height < min_vol:
+            self.bar_height = min_vol
 
         # draws visual
         cv2.rectangle(img, (20, 20), (40, 120), (255, 127, 0), 2)
-        cv2.rectangle(img, (20, 120 - self.volume), (40, 120), (255, 127, 0), -1)
+        cv2.rectangle(img, (20, 120 - self.bar_height), (40, 120), (255, 127, 0), -1)
 
     def get_angle(self, x1, y1, x2, y2, x3, y3):
         angle = 0
